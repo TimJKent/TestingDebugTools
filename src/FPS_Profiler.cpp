af57a38c0e2f77a8406d519fe80abe46d949674f
TimJKent <59515387+TimJKent@users.noreply.github.com>
2024-08-11 21:27:15 -0500

    Added queue to fps

--- src/FPS_Profiler.cpp
+++ src/FPS_Profiler.cpp
@@ -1,9 +1,10 @@
 #include <T_DEBUG_TOOLS/FPS_Profiler.h>
+#include <queue>
 
 namespace TDT
 {
-    
-static uint32_t fps = 0; 
+
+static std::queue<uint32_t> fpsQueue;
 
 FPS_Profiler::FPS_Profiler()
 {
@@ -13,11 +14,25 @@ FPS_Profiler::FPS_Profiler()
 FPS_Profiler::~FPS_Profiler()
 {
     std::chrono::milliseconds funcRuntimeMs = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - startTime);
-    fps = (1.0 / funcRuntimeMs.count()) * 1000.0;
+    fpsQueue.push((1.0 / funcRuntimeMs.count()) * 1000.0);
+    if(fpsQueue.size() > 10)
+    {
+        fpsQueue.pop();
+    }
 }
 
 uint32_t FPS_Profiler::GetFPS(){
-    return fps;
+    uint32_t fps = 0;
+    std::queue<uint32_t> copy_queue = fpsQueue;
+    int count = 0;
+    while(!copy_queue.empty())
+    {
+        count ++;
+        fps+= copy_queue.front();
+        copy_queue.pop();
+    }
+    if(count == 0){return 0;}
+    return fps/count;
 }
 
 }
